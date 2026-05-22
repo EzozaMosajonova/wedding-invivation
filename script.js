ab26af0498e67a1a493c2c8a10c7d8fdf22e0734
Fesleen <mosajonovae@gmail.com>
2026-05-22 22:42:13 +0500

    update wedding page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const weddingDate = new Date('August 10, 2025 11:00:00').getTime();
+const weddingDate = new Date('June 03, 2026 10:00:00').getTime();
 
 function updateCountdown() {
     const now = new Date().getTime();
